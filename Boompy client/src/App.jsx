59d5e9a7e36489de02a00ec047beb38ebf9013c2
danielcruz94 <daniel94cruz@gmail.com>
2024-04-25 14:59:30 -0500

    actualizacion cambios

--- Boompy client/src/App.jsx
+++ Boompy client/src/App.jsx
@@ -4,21 +4,15 @@ import './App.css'
 import Signup from "./views/SignUp/Signup";
 //libraries
 
-<<<<<<< HEAD
 import { Routes, Route,useLocation,useNavigate} from 'react-router-dom';
 import Login from "./views/Login";
 import Home from "./views/Home"
 
-=======
-import Landing from "../src/views/Landing"
-import './App.css'
->>>>>>> 95af5b1a1138b77676d671a260bf1fbf7573c206
 
 function App() {
  
 
   return (
-<<<<<<< HEAD
     <Routes>
     <Route path='/login' element={<Login></Login>}/>
     <Route path='/home' element={<Home></Home>}/>
@@ -27,9 +21,6 @@ function App() {
       
 
     </Routes>
-=======
-   <Landing></Landing>
->>>>>>> 95af5b1a1138b77676d671a260bf1fbf7573c206
     
     
   )
