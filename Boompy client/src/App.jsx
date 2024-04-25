8665bdadccdad478080b98c1ed4bc89279fe2ad3
Daniel Esteban Cruz Aristizabal <77306076+danielcruz94@users.noreply.github.com>
2024-04-25 14:44:38 -0500

    Revert "Boompy"

--- Boompy client/src/App.jsx
+++ Boompy client/src/App.jsx
@@ -1,24 +1,12 @@
 
-//import Landing from "../src/views/Landing"
-
-import Nav from "../src/shared/NavBar/NavBar";
-//import Home from "./shared/Components/Home/Home";
-import Error from "./shared/Components/Error/Error";
-import Footer from "./shared/Components/Footer/Footer"
-
+import Landing from "../src/views/Landing"
 import './App.css'
 
 function App() {
  
 
   return (
-  // <Landing></Landing>
-  <div>
-    <Nav/>
-    <Error/>
-    <Footer/>
-  </div>
-   
+   <Landing></Landing>
     
     
   )
