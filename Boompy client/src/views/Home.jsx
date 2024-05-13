fef3a833d3b5f1556a2085d39dcb0e09587d23da
danielcruz94 <daniel94cruz@gmail.com>
2024-05-13 17:56:04 -0500

    se corrigio el nombre del setitem a userdata

--- Boompy client/src/views/Home.jsx
+++ Boompy client/src/views/Home.jsx
@@ -76,7 +76,7 @@ const Home = () => {
   //fucntions
 
   const handleLogout = () => {
-    window.localStorage.removeItem("loggedAppUser");
+    window.localStorage.removeItem("userData");
     navegate("/");
   };
 
