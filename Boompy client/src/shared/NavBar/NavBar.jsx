23dbf581b48eb76ef1a4ec23f456b2ad23e805fd
Waltza1 <111610017+Waltza1@users.noreply.github.com>
2024-07-03 18:14:58 -0500

    Home NavBar

--- Boompy client/src/shared/NavBar/NavBar.jsx
+++ Boompy client/src/shared/NavBar/NavBar.jsx
@@ -1,106 +1,75 @@
 import React, { useState } from 'react';
-import { 
-    ContainerBar, 
-    ContainerNavBar, 
-    Image, 
-    SubHeading, 
-    SubmitButton, 
-    Bottom 
-} from '../../views/Landing.style';
-
-import {logout} from '../../Redux/authSlice'
-import { useSelector,useDispatch } from "react-redux"
-import { useNavigate} from 'react-router-dom';
- 
-import Vector from '../../assets/Vector.svg'
-
-
-import { Link } from 'react-router-dom';
-import StudentCalendar from '../Components/Calendar/Student_Calendar'; 
-
-import CalendarClass from '../Components/Calendar/Calendar_Class'; 
+import { ContainerBar, ContainerNavBar, Image, SubHeading, SubmitButton, Bottom } from '../../views/Landing.style';
+import { useSelector } from "react-redux";
+import { useNavigate, Link, useLocation } from 'react-router-dom';
+import StudentCalendar from '../Components/Calendar/Student_Calendar';
+import CalendarClass from '../Components/Calendar/Calendar_Class';
 import Carrito from '../../assets/carrito.svg';
-import Corazon from '../../assets/corazon.svg'
-import  Button  from '../../assets/Button.svg'
-
+import Corazon from '../../assets/corazon.svg';
+import Button from '../../assets/Button.svg';
+import Vector from '../../assets/Vector.svg'; // Asegúrate de importar Vector correctamente
 
 const NavBar = ({ textBotton, onClick, userInfo }) => {
-  const [isCalendarOpen, setIsCalendarOpen] = useState(false); 
+  const [isCalendarOpen, setIsCalendarOpen] = useState(false);
+  const userDataString = localStorage.getItem('userData');
+  const userData = JSON.parse(userDataString);
+  const [role, setRole] = useState(userData.role);
 
- 
-const userDataString = localStorage.getItem('userData');
-const userData = JSON.parse(userDataString);
-
-  const [role, setRole] = useState(userData.role); 
-console.log('role',role)
-  
-
-  const auth = useSelector((state) => state.auth);
- const dispatch=useDispatch()
- const navegate =useNavigate()
-
- 
- 
-
-
+  const navigate = useNavigate();
+  const location = useLocation();
 
   const toggleCalendar = () => {
-    setIsCalendarOpen(!isCalendarOpen); 
+    setIsCalendarOpen(!isCalendarOpen);
   };
 
   const closeCalendar = () => {
     setIsCalendarOpen(false);
   };
 
-  
-  const handleRoleChange = (newRole) => {
-    setRole(newRole);
-   
-    closeCalendar();
-  };
-
-  
   const CalendarComponent = role === 'Tutor' ? CalendarClass : StudentCalendar;
+
+  // Verifica si la ruta actual coincide con '/calls/:id'
+  const isCallsActive = location.pathname.startsWith('/calls/');
 
   return (
     <ContainerBar>
       <Image>
-        <img src="https://res.cloudinary.com/danielcruz/image/upload/v1716001393/images/c5bvvai78sokoqml83fk.png" style={{width:'50px'}} alt="logo" />
+        <img src="https://res.cloudinary.com/danielcruz/image/upload/v1716001393/images/c5bvvai78sokoqml83fk.png" style={{ width: '50px' }} alt="logo" />
         <b><p>{userInfo?.name}</p></b>
       </Image>
       <div style={{ display: 'flex' }}>
-        <Link to={"/home"}>
-          {role==='Tutor'?"":<SubHeading style={{color:'black',fontWeight: 'bold'}}>Home</SubHeading>}
-          
-        </Link>
-        {/* <SubHeading>Messages</SubHeading> */}
-        <SubHeading 
-          onClick={toggleCalendar} 
+        {/* Condicional para mostrar el enlace a '/home' o un texto inactivo */}
+        {isCallsActive ? (
+          <SubHeading style={{ color: 'grey', fontWeight: 'bold' }}>Home</SubHeading>
+        ) : (
+          <Link to={"/home"}>
+            {role === 'Tutor' ? "" : <SubHeading style={{ color: 'black', fontWeight: 'bold' }}>Home</SubHeading>}
+          </Link>
+        )}
+        {/* Elemento para mostrar el calendario */}
+        <SubHeading
+          onClick={toggleCalendar}
           style={{ cursor: 'pointer', fontWeight: 'bold' }}
         >
           Calendar
-        </SubHeading> 
+        </SubHeading>
       </div>
 
       <ContainerNavBar>
-       
+        {/* Elementos adicionales del NavBar */}
         <img src={Vector} alt="vector" />
-       
-      
-       
         <p style={{ marginLeft: '6px' }}>Categories</p>
         <SubmitButton placeholder="Search your partner"></SubmitButton>
         <img src={Button} style={{ width: '10px' }} alt="button" />
       </ContainerNavBar>
+      {/* Sección inferior del NavBar */}
       <div style={{ display: 'flex', gap: '5px' }}>
         <img src={Corazon} alt="corazon" />
         <img src={Carrito} alt="carrito" />
-
         <Bottom onClick={onClick}>{textBotton}</Bottom>
-
-
       </div>
 
+      {/* Componente del calendario que se muestra si isCalendarOpen es true */}
       {isCalendarOpen && <CalendarComponent isOpen={isCalendarOpen} onRequestClose={closeCalendar} onClose={closeCalendar} />}
     </ContainerBar>
   );
