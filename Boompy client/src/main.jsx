51afc2efc48fa78d5ad53445ab48bbe22792497b
danielcruz94 <daniel94cruz@gmail.com>
2024-07-07 21:25:15 -0500

    prueba de netlify

--- Boompy client/src/main.jsx
+++ Boompy client/src/main.jsx
@@ -11,7 +11,7 @@ import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
  <Provider store={store}>
 
-    <BrowserRouter basename="/">
+    <BrowserRouter>
     <App />
     </BrowserRouter>
 
