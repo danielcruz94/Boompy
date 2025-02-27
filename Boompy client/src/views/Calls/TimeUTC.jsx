303b6a7e6695303da85a56479a26579550cf1114
Waltza1 <111610017+Waltza1@users.noreply.github.com>
2025-02-27 15:56:28 -0500

    api UTC actualizada

--- Boompy client/src/views/Calls/TimeUTC.jsx
+++ Boompy client/src/views/Calls/TimeUTC.jsx
@@ -1,14 +1,12 @@
-
 import axios from 'axios';
 
 export async function TimeUTC() {
     try {
-        const response = await axios.get('https://worldtimeapi.org/api/ip');
+        const response = await axios.get('https://www.timeapi.io/api/Time/current/zone?timeZone=UTC');
         if (response.status !== 200) {
             throw new Error('Error al obtener la hora UTC desde el servidor');
         }        
-        const horaUTC = response.data.utc_datetime;
-        return horaUTC;
+        return response.data.dateTime; 
     } catch (error) {
         console.error('Error al obtener la hora UTC:', error);
         throw error; 
