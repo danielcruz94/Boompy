21d77a0597941f6ec8e82ad07d8c97573ced9391
danielcruz94 <daniel94cruz@gmail.com>
2025-03-18 18:22:02 -0500

    correccion carga de img inicio session

--- Boompy client/src/views/Login.jsx
+++ Boompy client/src/views/Login.jsx
@@ -1,39 +1,34 @@
-
 import Form from '../shared/Components/FormLogin/Form'
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import Spinner from "../../src/shared/Components/Modals/Spinners/Spinner";
 
+const Login = () => {
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    const img = new Image();
+    img.src = "/login/fondo.png";
+    img.onload = () => setIsLoading(false);
+  }, []);
 
-const Login=() => {
-    return (
-   
-      <div className="container-login">
-        <img src="/login/logo.png" alt="TORII" className="form_logo" />
-        <img src="/login/pqrs.png" alt="TORII" className="form_pqrs" />    
-         
-    <Form></Form>
-
-   
-      <a href="/signup" >
-      <img src="/login/Vector 1.png" alt="TORII" className="form_vector" />
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <div className="container-login">
+      <img src="/login/fondo.png" alt="fondo" className="hidden" />
+      <img src="/login/logo.png" alt="TORII" className="form_logo" />
+      <img src="/login/pqrs.png" alt="TORII" className="form_pqrs" />    
+      <Form />
+      <a href="/signup">
+        <img src="/login/Vector 1.png" alt="TORII" className="form_vector" />
       </a>
-    
-
-      <p className='Tex_condiciones '>© 2024 Torii. All rights reserved | <Link to="/service">Terminos y Condiciones</Link> | <Link to="/privacy">Politica de privacidad de datos</Link></p>    
-      </div>
-    
-
-   
-   
-
-    
- 
-
-
-
-    
-    
-    )
+      <p className="Tex_condiciones">
+        © 2024 Torii. All rights reserved | <Link to="/service">Términos y Condiciones</Link> | <Link to="/privacy">Política de privacidad de datos</Link>
+      </p>    
+    </div>
+  );
 }
 
 export default Login;
+
