c77ada4dc7925916f924db0647d23c656c79150b
danielcruz94 <daniel94cruz@gmail.com>
2025-03-27 13:17:14 -0500

    calendarios css cambios

--- Boompy client/src/shared/Components/Calendar/Calendar_Class.jsx
+++ Boompy client/src/shared/Components/Calendar/Calendar_Class.jsx
@@ -401,15 +401,17 @@ hoursOptions.push('12:00 AM');
         </div>
       </div>
 
-      <div className='Conten-Clases'>
+      <div className='Conten-Time-Class'>
   { /*<p>Available Hours for {selectedDate.toLocaleDateString()}</p>*/ }
+  {availableHoursForDate.length>0&&<p className='tituloCalendar'>Horas disponibles:</p>}
   
-  <div className="time-slots-container">
+  <div className="cuadroHoras">
     {availableHoursForDate.map(({ _id, startTime, endTime, reserved, cancel }, index) => (
-      <div key={index} className={`time-slot ${reserved ? 'reserved' : ''} ${cancel ? 'cancel' : ''}`}>
+      <div key={index} className={`class-info time-slot div-Class ${reserved ? 'reserved' : ''} ${cancel ? 'cancel' : ''}`}>
         
-        <div className="time-range">
-          <span>{formatTime(startTime)}</span> - <span>{formatTime(endTime)}</span>
+        <div className="InfoClass">
+          {/* <span>{formatTime(startTime)}</span> - <span>{formatTime(endTime)}</span> */}
+          <p className='formatoHoras'>{formatTime(startTime)}</p>
         </div>
         
         {reserved && (
@@ -420,9 +422,9 @@ hoursOptions.push('12:00 AM');
           </div>
         )}
 
-           <div className='Canceldiv'>
-               <button className="cancelButton"  onClick={() => cancelClass(_id,startTime)}>X</button>
-           </div>
+           {/* <div className='Canceldiv'>
+               <p className=""  onClick={() => cancelClass(_id,startTime)} style={{width:'0.5px',height:'0.5px'}}>X</p>
+           </div> */}
         
       </div>
     ))}
